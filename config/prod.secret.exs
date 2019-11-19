f38333e2e3b0e30d4b1b39ecdea3b309946c39b4
Felipe <duzzifelipe@gmail.com>
2019-11-19 11:31:47 -0300

    fix: move server config to prod secrets

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -25,7 +25,8 @@ secret_key_base =
 
 config :stonex, StonexWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  server: true
 
 # ## Using releases (Elixir v1.9+)
 #
