8f126fd391a57ff17297121f112145c4c27e1a1e
Felipe <duzzifelipe@gmail.com>
2019-11-19 11:00:01 -0300

    chore: start endpoint as server

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :stonex, StonexWeb.Endpoint,
+  server: true,
   url: [host: "example.com", port: 80],
 
 # Do not print debug messages in production
