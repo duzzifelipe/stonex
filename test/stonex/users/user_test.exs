c455ead56853c9a05e8c8e29596dc27e38e29a72
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2019-11-20 08:38:52 -0300

    test: specify tests for user schema

---
+++ test/stonex/users/user_test.exs
@@ -0,0 +1,27 @@
+defmodule Stonex.UserTest do
+  use Stonex.DataCase
+
+  alias Stonex.User
+
+  describe "users" do
+    test "signup_changeset/2 with valid parameters"
+
+    test "signup_changeset/2 with no arguments"
+
+    test "signup_changeset/2 without first name"
+
+    test "signup_changeset/2 without registration_id"
+
+    test "signup_changeset/2 without email"
+
+    test "signup_changeset/2 with wrong email format"
+
+    test "signup_changeset/2 with small password"
+
+    test "signup_changeset/2 without password"
+
+    test "signup_changeset/2 without password confirmation"
+
+    test "signup_changeset/2 with password not matching"
+  end
+end
