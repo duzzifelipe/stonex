3667bed4d02ff36c74b67ab58224ae274dad8db9
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2019-11-19 21:37:37 -0300

    feat: user schema definition

---
+++ lib/stonex/users/user.ex
@@ -0,0 +1,22 @@
+defmodule Stonex.User do
+  @moduledoc """
+  This module holds the database structure
+  and logic for bank users (clients).
+
+  Operations made by an user and handled by user
+  schema logic are registration and authentication
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field(:first_name, :string, null: false)
+    field(:last_name, :string)
+    field(:registration_id, :string, null: false)
+    field(:email, :string, null: false)
+    field(:encrypted_password, :string, null: false)
+
+    timestamps()
+  end
+end
