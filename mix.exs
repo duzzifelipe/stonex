09511c3e4b96c841302770c2b4879e2e19ec7265
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2019-11-20 13:30:04 -0300

    chore: add cpf validator library

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Stonex.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:faker, "~> 0.13", only: :test},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:cpf, "~> 1.0"}
     ]
   end
 
