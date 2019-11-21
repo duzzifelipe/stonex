8b3b01eda5df5bc330a305680ef15ab2d883fc26
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2019-11-21 19:35:59 -0300

    feat: implement account history changeset

--- lib/stonex/accounts/account_history.ex
+++ lib/stonex/accounts/account_history.ex
@@ -21,4 +21,34 @@ defmodule Stonex.Accounts.AccountHistory do
 
     timestamps()
   end
+
+  @doc """
+  Receive attributes that describe an account't transaction,
+  including operation type (credit or debit) and the value
+
+  ## Examples
+
+      iex> changeset = Stonex.Accounts.AccountHistory.create_changeset(
+      ...>   %Stonex.Accounts.AccountHistory{},
+      ...>   %{type: "debit", account_id: 1, amount: 3000}
+      ...> )
+      ...> changeset.valid?
+      true
+
+      iex> changeset = Stonex.Accounts.AccountHistory.create_changeset(
+      ...>   %Stonex.Accounts.AccountHistory{},
+      ...>   %{type: "loan", account_id: 1, amount: 200}
+      ...> )
+      ...> changeset.valid?
+      false
+  """
+  @spec create_changeset(Stonex.Accounts.AccountHistory.t(), map) :: Ecto.Changeset.t()
+  def create_changeset(%__MODULE__{} = account_history, attrs) do
+    account_history
+    |> cast(attrs, [:type, :amount, :account_id])
+    |> validate_required([:type, :amount, :account_id])
+    |> foreign_key_constraint(:account_id)
+    |> validate_inclusion(:type, ~w(credit debit))
+    |> validate_number(:amount, greater_than: 0)
+  end
 end
